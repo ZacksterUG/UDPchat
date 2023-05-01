dcddcb80271f4b0219d83cbd17d06a8025b550be
Фоменко Алексей <AlFomenko@croc.ru>
2023-05-01 13:56:18 +0500

    initial commit

---
+++ client/index.ts
@@ -0,0 +1,24 @@
+import { ClientChat } from "./source/ClientChat";
+import { app, BrowserWindow } from 'electron';
+import path from 'path';
+
+const clientChat = new ClientChat("Zackster");
+clientChat.startReceiving(200);
+
+app.whenReady().then(() => {
+    const win = new BrowserWindow({
+        show: false,
+        webPreferences: {
+            preload: path.join(__dirname, '\\source\\preload.js'),
+            nodeIntegration: true,
+            contextIsolation: true,
+        }
+    });
+
+    win.loadFile('index.html');
+    win.once('ready-to-show', win.show);
+});
+
+app.on('window-all-closed', app.quit);
+
+
